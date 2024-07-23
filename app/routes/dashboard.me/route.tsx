1ca949fff00e234fbbb492dfb05c407d981f4750
Nahuel Luca <nahuelluca20@gmail.com>
2024-07-23 19:08:22 -0300

    chore: Add conditional rendering for user role in dashboard route

--- app/routes/dashboard.me/route.tsx
+++ app/routes/dashboard.me/route.tsx
@@ -2,5 +2,13 @@ import { useOutletContext } from "@remix-run/react";
 
 export default function Route() {
   const role: string = useOutletContext();
-  return <div>{role}</div>;
+  return (
+    <div>
+      {role === "admin" ? (
+        <h1>Hello you are a Admin</h1>
+      ) : (
+        <h1>Hello you are a User</h1>
+      )}
+    </div>
+  );
 }
